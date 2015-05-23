0d2b11e65a777d33da2e08b9ae94ee6379ffd27e
Maxime Fourré <maxime62750@gmail.com>
2015-05-23 20:58:21 +0200

    debut inscription

---
+++ www/js/controllers/register.js
@@ -0,0 +1,8 @@
+controllers.controller('RegisterCtrl', function($scope, $http, $state, $cordovaDevice, $cordovaKeychain){
+
+    //Inscription
+    $scope.register = function(user){
+        console.log("hop hop hop");
+    }
+
+});

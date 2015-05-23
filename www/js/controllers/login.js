432a9cb1380c80183556d5cabe56fb80a8bcc5be
Thomas Geselle <raitomm@gmail.com>
2015-05-23 18:46:54 +0200

    Login

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -1,3 +1,45 @@
-controllers.controller('LoginCtrl', function(){
+controllers.controller('LoginCtrl', function($scope, $http, $state, $cordovaDevice, $cordovaKeychain){
+
+    //Connexion
+    $scope.login = function(user){
+
+        if(isEmpty(user['password']) || isEmpty(user['email'])){
+            $scope.errors = "Tous les champs doivent êtres remplis";
+        }else{
+            var password = CryptoJS.SHA3(user['password']).toString(CryptoJS.enc.Base64);
+            var email = user['email'];
+
+            $http({
+                url: "http://92.222.82.233/users",
+                method: "GET",
+                params: {email: email, password: password}
+            }).error(function(data){
+                $scope.errors = "Erreur réseau";
+            }).success(function(data){
+                if(data['id'] === null){
+                    //Redirection vers l'écran de connexion
+                    $scope.errors = "Identifiants invalides"
+                }else{
+                    //L'utilisateur est connecté, enregistrement de ses identifiants dans le Keychain ou LocalStorage
+                    if($cordovaDevice.getPlatform() === "iOS"){
+                        var kc = new Keychain();
+                        kc.setForKey(function(){}, function(){}, 'email', 'buffalo', email);
+                        kc.setForKey(function(){
+                            $state.go('main');
+                        }, function(){}, 'password', 'buffalo', password);
+                    }else{
+                        window.localStorage['email'] = email;
+                        window.localStorage['password'] = password;
+                        $state.go('main');
+                    }
+                }
+            });
+        }
+
+    };
+
+    function isEmpty(str) {
+        return (!str || 0 === str.length);
+    }
 
 });

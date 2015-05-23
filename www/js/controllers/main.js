5d0399c1b1ab43e79dc89a364bcefa5d1ac0c11c
Thomas Geselle <raitomm@gmail.com>
2015-05-23 19:59:13 +0200

    Tinder like cards

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -3,53 +3,71 @@ controllers.controller('MainCtrl', function($scope, $ionicPlatform, $cordovaDevi
     $ionicPlatform.ready(function() {
         //Vérification si l'utilisateur a déjà utilisé l'app
         //Si oui alors les identifiants de l'utilisateur (email et password) doivent être stocké dans l'app
-        var email = "";
-        var password = "";
+        login();
+    });
+
+    $scope.cards = [
+        {name: "test", url: "http://www.menucool.com/slider/prod/image-slider-2.jpg"},
+        {name: "test2", url: "http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg"}
+    ];
+
+    $scope.cardDestroyed = function(index) {
+        $scope.cards.splice(index, 1);
+    };
+
+    $scope.cardSwiped = function(index) {
+        var newCard = // new card data
+        $scope.cards.push(newCard);
+    };
+
+
+
+    function login(){
+        $scope.email = "";
+        $scope.password = "";
 
         //Si ios : Stockage dans le Keychain (stockage sécurisé et encrypté)
         if($cordovaDevice.getPlatform() === "iOS"){
             var kc = new Keychain();
 
             kc.getForKey(function(value){
-                console.log("Email fetched");
-                email = value;
+                $scope.email = value;
 
                 kc.getForKey(function(value){
-                    console.log("Password fetched");
-                    password = value;
+                    $scope.password = value;
+
+                    sendRequestLogin($scope.email, $scope.password);
                 }, function(error){
-                    console.log(error)
+                    $state.go('homeScreen');
                 }, "password", "buffalo")
             }, function(error){
-                console.log(error)
+                $state.go('homeScreen');
             }, "email", "buffalo");
         }else{
             //Sinon récupération dans le localStorage
-            email = window.localStorage['email'] || "";
-            password = window.localStorage['password'] || "";
+            $scope.email = window.localStorage['email'] || "";
+            $scope.password = window.localStorage['password'] || "";
+            sendRequestLogin($scope.email, $scope.password);
         }
+    }
 
-        //Vérification si les identifiants sont corrects avec un appel API (doit renvoyé l'id user)
-        password = CryptoJS.SHA3(password).toString(CryptoJS.enc.Base64);
-
+    //Vérification si les identifiants sont corrects avec un appel API (doit renvoyé l'id user)
+    function sendRequestLogin(email, password){
         $http({
             url: "http://92.222.82.233/users",
             method: "GET",
             params: {email: email, password: password}
         }).error(function(data){
-            console.log("Erreur réseau");
             $state.go('homeScreen')
         }).success(function(data){
             if(data['id'] === null){
                 //Redirection vers l'écran de connexion
-                console.log("Id non trouvé");
                 $state.go("homeScreen")
+                $scope.user_id = data
             }else{
                 //L'utilisateur est connecté
-                console.log("Connecté !");
-                $scope.user_id = data;
+                $scope.user_id = data['id'];
             }
         });
-
-    });
+    }
 });

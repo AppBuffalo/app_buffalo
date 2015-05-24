7e3af5226970ff339f575f13cf827c2dfccf9382
unknown <thomas.sarek@gmail.com>
2015-05-24 18:53:30 +0200

    bop bop bop

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -70,4 +70,42 @@ controllers.controller('MainCtrl', function($scope, $ionicPlatform, $cordovaDevi
             }
         });
     }
+        $scope.creds = {
+            bucket: 'sarekzamel',
+            access_key: 'AKIAJ66UUBRJQEXA27EQ',
+            secret_key: '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'
+        }
+
+        $scope.upload = function() {
+            console.log("called");
+            // Configure The S3 Object
+            AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
+            AWS.config.region = 'eu-west-1';
+            var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
+
+            if($scope.file) {
+                var params = { Key: $scope.file.name, ContentType: $scope.file.type, Body: $scope.file, ServerSideEncryption: 'AES256' };
+
+                bucket.putObject(params, function(err, data) {
+                    if(err) {
+                        // There Was An Error With Your S3 Config
+                        console.log(err.message);
+                        return false;
+                    }
+                    else {
+                        // Success!
+                        console.log('Upload Done');
+                    }
+                })
+                    .on('httpUploadProgress',function(progress) {
+                        // Log Progress Information
+                        console.log(Math.round(progress.loaded / progress.total * 100) + '% done');
+                    });
+            }
+            else {
+                // No File Selected
+                console.log('No File Selected');
+            }
+        }
+
 });
---
+++ www/js/services/upload.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Thomas on 24/05/2015.
+ */
+
+angular.module('buffalo')
+    .service("Upload", function($http, $q, $rootscope){
+
+
+});

88628163d7714838c428deb9bb97e6fa20ccaf1d
Thomas Geselle <raitomm@gmail.com>
2015-05-25 22:39:10 +0200

    Upload photos

--- www/js/controllers/comment.js
+++ www/js/controllers/comment.js
@@ -1,3 +1,40 @@
-controllers.controller('CommentCtrl', function($scope, $stateParams){
-    $scope.image_uri = $stateParams.imageURI;
+controllers.controller('CommentCtrl', function($scope, $stateParams, $ionicPlatform, $state,
+                                               $ionicViewSwitcher, $cordovaFile, $cordovaFileTransfer){
+
+    $ionicPlatform.ready(function() {
+        $scope.image_uri = $stateParams.imageURI;
+        document.getElementById("form-comment").focus();
+    });
+
+    $scope.ajouterCommentaire = function(){
+        if(document.getElementById("form-comment").style.visibility === "visible"){
+            document.getElementById("form-comment").style.visibility = "hidden";
+        }else{
+            document.getElementById("form-comment").style.visibility = "visible";
+            document.getElementById("form-comment").click().focus();
+        }
+    };
+
+    $scope.retourMain = function(){
+        $ionicViewSwitcher.nextDirection('back');
+        $state.go("main");
+    };
+
+    $scope.envoyerPhoto = function(){
+        console.log("Envois Photo");
+
+        var uploadOptions = {
+            params : { 'upload_preset': "hfrgicap"}
+        };
+
+        $cordovaFileTransfer
+            .upload("cloudinary://319843273551643:cFGMpedaTrczjCgQUhgUmbQLHCw@dbqbmbcvg", $scope.image_uri, uploadOptions)
+            .then(function(result) {
+                console.log("OK");
+            }, function(err) {
+                console.log(err);
+            });
+
+    };
 });
+
